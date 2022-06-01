95f578d5ef9a74c82dca44da6862e8ae2c68b64c
Pedro Morais <pedrojfmorais@hotmail.com>
2022-06-01 14:20:52 +0100

    control panel e tabela a funcionar

--- sopa-letras/src/App.js
+++ sopa-letras/src/App.js
@@ -1,15 +1,292 @@
-import Header from './components/header/header.component';
-import Footer from './components/footer/footer.component';
 import "./assets/styles/app.css"
-import GameTable from './components/game-table/game-table.component';
+import { useState, useEffect } from "react";
+
+import {
+  Header,
+  Footer,
+  ControlPanel,
+  GameTable,
+} from "./components";
 
 function App() {
+
+  
+  let timerId = undefined;
+  
+  const [gameStarted, setGameStarted] = useState(false);
+  const [selectedLevel, setSelectedLevel] = useState("0");
+  const [tabelaJogo, setTabelaJogo] = useState([[]]);
+  const [palavrasEmJogo, setPalavrasEmJogo] = useState([]);
+  const [timer, setTimer] = useState(20);
+
+  useEffect(() => { 
+    if (gameStarted) { 
+      timerId = setInterval(() => { 
+        setTimer(timer-1); 
+        let nextTimer = timer - 1; 
+        if (nextTimer === 0) { 
+          setGameStarted(false); 
+        } 
+      }, 1000); 
+    } else if (timer !== 20) { 
+      setTimer(20); 
+    } 
+    return () => { 
+      if (timerId) { 
+        clearInterval(timerId); 
+      } 
+    }; 
+  }, [gameStarted, timer]);
+
+  const handleGameStart = () => {
+    if (gameStarted) {
+      console.log("Termina Jogo");
+      setGameStarted(false);
+    } else {
+      console.log("Inicia Jogo");
+      setGameStarted(true);
+    }
+  };
+
+  const handleLevelChange = (event) => {
+
+    const { value } = event.currentTarget;
+    setSelectedLevel(value);
+
+    const dificuldadeAtual = parseInt(value);
+
+    const palavrasPossiveis = ['REACT', 'JAVASCRIPT', 'HTML', 'CSS', 'BACKBONE', 'ANGULAR', 'SVELTE', 'EMBER', 'BOOTSTRAP', 'VUE', 'JAVA', 'PYTHON', 'SQL']; 
+    
+    const nPalavrasDificuldade = [5, 7, 9];
+    const nLinhasDificuldade = [10, 15, 20];
+    let tabelaJogoTemp = [[]];
+    let palavrasEmJogo = [];
+
+    function getRandomNumber(max){
+      return Math.floor(Math.random() * max);
+    }
+
+    function generateRandomLetter() {
+      var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      return characters.charAt(getRandomNumber(characters.length));
+    }
+    
+    function getRandomWord() {
+      return palavrasPossiveis[getRandomNumber(palavrasPossiveis.length)];
+    }
+    
+    function preencheTabelaJogoComLetrasRandom(){
+      tabelaJogoTemp = [[]];
+      tabelaJogoTemp = Array(nLinhasDificuldade[dificuldadeAtual-1]).fill(0).map( 
+        () => new Array(nLinhasDificuldade[dificuldadeAtual-1]).fill(0).map( 
+          () => generateRandomLetter()
+          )
+        );
+    }
+
+    function geraPalavrasRandom(){
+
+      let palavras = [];
+
+      let novaPalavra = true;
+
+      for (let i = 0; i < nPalavrasDificuldade[dificuldadeAtual-1]; i++) {
+        let palavra = getRandomWord();
+
+        while(true){
+          novaPalavra = true;
+          for (let index = 0; index < palavras.length; index++)
+            if(palavras[index] === palavra || palavra.length > nLinhasDificuldade[dificuldadeAtual-1]){
+              novaPalavra = false;
+              palavra = getRandomWord();
+              break;
+            }
+            
+          if(novaPalavra)
+            break;
+          
+        }
+
+        palavras.push(palavra);
+      }
+      return palavras;
+    }
+
+    
+    function geraComoColocarNaTabela(palavras){
+
+      let palavrasEmJogoDados = [];
+      let celulasOcupadas = [];
+
+      palavras.forEach(palavra => {
+
+        const nLinhas = nLinhasDificuldade[dificuldadeAtual-1];
+
+        let linha = getRandomNumber(nLinhas);
+        let coluna = getRandomNumber(nLinhas);
+        
+        let orientacao = getRandomNumber(8);
+
+        while (true) {
+          
+          let conseguiuColocar = true;
+          let celulasOcupadasParaEste = [];
+      
+          linha = getRandomNumber(nLinhas);
+          coluna = getRandomNumber(nLinhas);
+          
+          orientacao = getRandomNumber(8);
+      
+          if(orientacao % 2 === 0)
+            palavra = palavra.split("").reverse().join("");
+
+          orientacao = Math.floor(orientacao / 2);
+          
+          switch (orientacao) {
+            case 1:
+              // cima baixo
+
+              if(linha + palavra.length >= nLinhasDificuldade[dificuldadeAtual-1])
+                linha = nLinhasDificuldade[dificuldadeAtual-1] - palavra.length;
+
+              for (let index = 0; index < palavra.length; index++)
+                celulasOcupadasParaEste.push([linha+index, coluna]);
+              break;
+
+            case 2:
+              // diagonal esquerda cima -> direita baixo
+
+              if(linha + palavra.length >= nLinhasDificuldade[dificuldadeAtual-1])
+                linha = nLinhasDificuldade[dificuldadeAtual-1] - palavra.length;
+
+              if(coluna + palavra.length >= nLinhasDificuldade[dificuldadeAtual-1])
+                coluna = nLinhasDificuldade[dificuldadeAtual-1] - palavra.length;
+                
+              for (let index = 0; index < palavra.length; index++)
+                celulasOcupadasParaEste.push([linha+index, coluna+index]);
+              break;
+
+            case 3:
+              // diagonal esquerda baixo -> direita cima
+              
+              if(linha - palavra.length <= -1)
+                linha = palavra.length - 1; // porque o index começa em 0
+
+              if(coluna + palavra.length >= nLinhasDificuldade[dificuldadeAtual-1])
+                coluna = nLinhasDificuldade[dificuldadeAtual-1] - palavra.length;
+
+                  
+              for (let index = 0; index < palavra.length; index++) 
+                celulasOcupadasParaEste.push([linha-index, coluna+index]);
+              break;
+
+            default:
+              // esquerda direita
+
+              if(coluna + palavra.length >= nLinhasDificuldade[dificuldadeAtual-1])
+                coluna = nLinhasDificuldade[dificuldadeAtual-1] - palavra.length;
+
+              for (let index = 0; index < palavra.length; index++)
+                celulasOcupadasParaEste.push([linha, coluna+index]);
+              break;
+          }
+        
+          for (let index_palavra = 0; index_palavra < celulasOcupadas.length; index_palavra++) {
+            for (let index = 0; index < celulasOcupadas[index_palavra].length; index++) {
+              for (let index_celulas_este = 0; index_celulas_este < celulasOcupadasParaEste.length; index_celulas_este++) {
+                
+                if(celulasOcupadas[index_palavra][index][0] === celulasOcupadasParaEste[index_celulas_este][0] &&
+                    celulasOcupadas[index_palavra][index][1] === celulasOcupadasParaEste[index_celulas_este][1]){
+                  conseguiuColocar = false;
+                  break;
+                }
+              }
+              if(conseguiuColocar === false)
+                break;
+            }
+            if(conseguiuColocar === false)
+              break;
+          }
+          if(conseguiuColocar === true){
+            celulasOcupadas.push(celulasOcupadasParaEste);
+            palavrasEmJogoDados.push([linha, coluna, orientacao]);
+            break;
+          }
+        }
+      });
+      return palavrasEmJogoDados;
+    }
+
+    function colocaPalavraTabelaJogo(dados){
+
+      const [linha, coluna, orientacao, palavra] = dados;
+
+      switch (orientacao) {
+        case 1:
+          // cima baixo
+          for (let index = 0; index < palavra.length; index++)
+            tabelaJogoTemp[linha+index][coluna] = palavra[index];
+          break;
+        case 2:
+          // diagonal esquerda cima -> direita baixo
+          for (let index = 0; index < palavra.length; index++)
+            tabelaJogoTemp[linha+index][coluna+index] = palavra[index];
+          break;
+
+        case 3:
+          // diagonal esquerda baixo -> direita cima
+          for (let index = 0; index < palavra.length; index++) 
+            tabelaJogoTemp[linha-index][coluna+index] = palavra[index];
+          break;
+
+          default:
+            // esquerda direita
+            for (let index = 0; index < palavra.length; index++)
+              tabelaJogoTemp[linha][coluna+index] = palavra[index];
+            break;
+      }
+
+    }
+
+    function colocaPalavrasTabelaJogo(){
+      
+      if (dificuldadeAtual === 0)
+        return;
+
+      preencheTabelaJogoComLetrasRandom();
+
+      palavrasEmJogo = geraPalavrasRandom();
+      palavrasEmJogo = palavrasEmJogo.sort((a, b) => b.length - a.length);
+
+      let comoColocarPalavrasTabela = geraComoColocarNaTabela(palavrasEmJogo);
+
+      for (let index = 0; index < palavrasEmJogo.length; index++) {
+        comoColocarPalavrasTabela[index].push(palavrasEmJogo[index]);
+        colocaPalavraTabelaJogo(comoColocarPalavrasTabela[index]);
+      }
+    }
+
+    colocaPalavrasTabelaJogo();
+
+    setTabelaJogo(tabelaJogoTemp);
+    setPalavrasEmJogo(palavrasEmJogo);
+  }
 
   return (
     <div id="container">
       <Header />
       <main className="main-content">
-        <GameTable />
+        <ControlPanel
+          gameStarted={gameStarted}
+          onGameStart={handleGameStart}
+          selectedLevel={selectedLevel}
+          onLevelChange={handleLevelChange}
+          timer={timer}
+        />
+        <GameTable 
+          tabelaJogo={tabelaJogo}
+          palavrasEmJogo={palavrasEmJogo}
+        />
       </main>
       <Footer />
     </div>
--- sopa-letras/src/components/game-table/game-table.component.jsx
+++ sopa-letras/src/components/game-table/game-table.component.jsx
@@ -1,247 +1,38 @@
 import React from "react";
-function GameTable() {
 
-    const palavrasPossiveis = ['REACT', 'JAVASCRIPT', 'HTML', 'CSS', 'BACKBONE', 'ANGULAR', 'SVELTE', 'EMBER', 'BOOTSTRAP', 'VUE', 'JAVA', 'PYTHON', 'SQL']; 
-    
-    const nPalavrasDificuldade = [5, 7, 9];
-    const nLinhasDificuldade = [10, 15, 20];
-    let tabelaJogo = [[]];
-    let palavrasEmJogo = [];
-  
-    const dificuldadeAtual = 1; //debug
+function GameTable(props) {
 
-    function getRandomNumber(max){
-      return Math.floor(Math.random() * max);
-    }
-  
-    function generateRandomLetter() {
-      var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-      return characters.charAt(getRandomNumber(characters.length));
-    }
-    
-    function getRandomWord() {
-      return palavrasPossiveis[getRandomNumber(palavrasPossiveis.length)];
-    }
-    
-    function preencheTabelaJogoComLetrasRandom(){
-      tabelaJogo = [[]];
-      tabelaJogo = Array(nLinhasDificuldade[dificuldadeAtual-1]).fill(0).map( 
-        () => new Array(nLinhasDificuldade[dificuldadeAtual-1]).fill(0).map( 
-          () => generateRandomLetter()
-          )
-        );
-    }
-  
-    function geraPalavrasRandom(){
+  const { tabelaJogo, palavrasEmJogo } = props;
 
-      let palavras = [];
-
-      let novaPalavra = true;
-
-      for (let i = 0; i < nPalavrasDificuldade[dificuldadeAtual-1]; i++) {
-        let palavra = getRandomWord();
-
-        while(true){
-          novaPalavra = true;
-          for (let index = 0; index < palavras.length; index++)
-            if(palavras[index] === palavra || palavra.length > nLinhasDificuldade[dificuldadeAtual-1]){
-              novaPalavra = false;
-              palavra = getRandomWord();
-              break;
-            }
-            
-          if(novaPalavra)
-            break;
-          
-        }
-
-        palavras.push(palavra);
-      }
-      return palavras;
-    }
-
-    
-    function geraComoColocarNaTabela(palavras){
-
-      let palavrasEmJogoDados = [];
-      let celulasOcupadas = [];
-
-      palavras.forEach(palavra => {
-
-        const nLinhas = nLinhasDificuldade[dificuldadeAtual-1];
-
-        let linha = getRandomNumber(nLinhas);
-        let coluna = getRandomNumber(nLinhas);
-        
-        let orientacao = getRandomNumber(8);
-
-        while (true) {
-          
-          let conseguiuColocar = true;
-          let celulasOcupadasParaEste = [];
-      
-          linha = getRandomNumber(nLinhas);
-          coluna = getRandomNumber(nLinhas);
-          
-          orientacao = getRandomNumber(8);
-      
-          if(orientacao % 2 === 0)
-            palavra = palavra.split("").reverse().join("");
-
-          orientacao = Math.floor(orientacao / 2);
-          
-          switch (orientacao) {
-            case 1:
-              // cima baixo
-
-              if(linha + palavra.length >= nLinhasDificuldade[dificuldadeAtual-1])
-                linha = nLinhasDificuldade[dificuldadeAtual-1] - palavra.length;
-
-              for (let index = 0; index < palavra.length; index++)
-                celulasOcupadasParaEste.push([linha+index, coluna]);
-              break;
-
-            case 2:
-              // diagonal esquerda cima -> direita baixo
-
-              if(linha + palavra.length >= nLinhasDificuldade[dificuldadeAtual-1])
-                linha = nLinhasDificuldade[dificuldadeAtual-1] - palavra.length;
-
-              if(coluna + palavra.length >= nLinhasDificuldade[dificuldadeAtual-1])
-                coluna = nLinhasDificuldade[dificuldadeAtual-1] - palavra.length;
-                
-              for (let index = 0; index < palavra.length; index++)
-                celulasOcupadasParaEste.push([linha+index, coluna+index]);
-              break;
-
-            case 3:
-              // diagonal esquerda baixo -> direita cima
-              
-              if(linha - palavra.length <= -1)
-                linha = palavra.length - 1; // porque o index começa em 0
-
-              if(coluna + palavra.length >= nLinhasDificuldade[dificuldadeAtual-1])
-                coluna = nLinhasDificuldade[dificuldadeAtual-1] - palavra.length;
-
-                  
-              for (let index = 0; index < palavra.length; index++) 
-                celulasOcupadasParaEste.push([linha-index, coluna+index]);
-              break;
-
-            default:
-              // esquerda direita
-
-              if(coluna + palavra.length >= nLinhasDificuldade[dificuldadeAtual-1])
-                coluna = nLinhasDificuldade[dificuldadeAtual-1] - palavra.length;
-
-              for (let index = 0; index < palavra.length; index++)
-                celulasOcupadasParaEste.push([linha, coluna+index]);
-              break;
-          }
-        
-          for (let index_palavra = 0; index_palavra < celulasOcupadas.length; index_palavra++) {
-            for (let index = 0; index < celulasOcupadas[index_palavra].length; index++) {
-              for (let index_celulas_este = 0; index_celulas_este < celulasOcupadasParaEste.length; index_celulas_este++) {
-                
-                if(celulasOcupadas[index_palavra][index][0] === celulasOcupadasParaEste[index_celulas_este][0] &&
-                    celulasOcupadas[index_palavra][index][1] === celulasOcupadasParaEste[index_celulas_este][1]){
-                  conseguiuColocar = false;
-                  break;
-                }
-              }
-              if(conseguiuColocar === false)
-               break;
-            }
-            if(conseguiuColocar === false)
-             break;
-          }
-          if(conseguiuColocar === true){
-            celulasOcupadas.push(celulasOcupadasParaEste);
-            palavrasEmJogoDados.push([linha, coluna, orientacao]);
-            break;
-          }
-        }
-      });
-      return palavrasEmJogoDados;
-    }
-
-    function colocaPalavraTabelaJogo(dados){
-
-      const [linha, coluna, orientacao, palavra] = dados;
-
-      switch (orientacao) {
-        case 1:
-          // cima baixo
-          for (let index = 0; index < palavra.length; index++)
-            tabelaJogo[linha+index][coluna] = palavra[index];
-          break;
-        case 2:
-          // diagonal esquerda cima -> direita baixo
-          for (let index = 0; index < palavra.length; index++)
-            tabelaJogo[linha+index][coluna+index] = palavra[index];
-          break;
-
-        case 3:
-          // diagonal esquerda baixo -> direita cima
-          for (let index = 0; index < palavra.length; index++) 
-            tabelaJogo[linha-index][coluna+index] = palavra[index];
-          break;
-
-          default:
-            // esquerda direita
-            for (let index = 0; index < palavra.length; index++)
-              tabelaJogo[linha][coluna+index] = palavra[index];
-            break;
-      }
-
-    }
-
-    function colocaPalavrasTabelaJogo(){
-      
-      preencheTabelaJogoComLetrasRandom();
-
-      palavrasEmJogo = geraPalavrasRandom();
-      palavrasEmJogo = palavrasEmJogo.sort((a, b) => b.length - a.length);
-
-      let comoColocarPalavrasTabela = geraComoColocarNaTabela(palavrasEmJogo);
-
-      for (let index = 0; index < palavrasEmJogo.length; index++) {
-        comoColocarPalavrasTabela[index].push(palavrasEmJogo[index]);
-        colocaPalavraTabelaJogo(comoColocarPalavrasTabela[index]);
-      }
-    }
-    
-    colocaPalavrasTabelaJogo();
-
-    return (
+  return (
+    <div>
       <div>
-        <div>
-            <table>
-          <thead>
-          </thead>
-          <tbody>
-            {tabelaJogo.map((items, index) => {
-              return (
-                <tr>
-                  {items.map((item, index) => {
-                    return (<td>{item}</td>);
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        </div>
-        <div>
-          {palavrasEmJogo.map((item, index) => {
-              return (
-                <p>
-                  {item}
-                </p>
-              );
-            })}
-        </div>
+          <table>
+        <thead>
+        </thead>
+        <tbody>
+          {tabelaJogo.map((items, index_linha) => {
+            return (
+              <tr key={index_linha}>
+                {items.map((item, index) => {
+                  return (<td key={index_linha + ' ' + index}>{item}</td>);
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
       </div>
-    );
+      <div>
+        {palavrasEmJogo.map((item, index) => {
+            return (
+              <p key={item}>
+                {item}
+              </p>
+            );
+          })}
+      </div>
+    </div>
+  );
 }
 export default GameTable;
